fa9a6984063084f8658cb7777e32ccd84f42da33
mghs15 <40787295+mghs15@users.noreply.github.com>
2020-04-17 22:34:18 +0900

    Add files via upload

--- hsl-style-color-analysis.pl
+++ hsl-style-color-analysis.pl
@@ -83,6 +83,14 @@ sub rgb2hsl {
 
 sub hsl2rgb {
 	my ($h, $s, $l) = @_;
+	
+	if( $h > 360){
+		$h = $h - 360*floor($h/360);
+	}elsif($h < 0){
+		$h = $h - 360*ceil($h/360);
+	}
+	
+	
 	my $part = 2*$l-1;
 	my $max = 0;
 	my $min = 0;
@@ -110,17 +118,21 @@ sub hsl2rgb {
 		$g = $max;
 		$b = $min + ($max-$min)*($h-120)/60;
 	}elsif($h < 240){
-		$r = $max;
+		$r = $min;
 		$g = $min + ($max-$min)*(240-$h)/60;
-		$b = $min;
+		$b = $max;
 	}elsif($h < 300){
-		$r = $min + ($max-$min)*($h-120)/60;
-		$g = $max;
-		$b = $min;
-	}else{
+		$r = $min + ($max-$min)*($h-240)/60;
+		$g = $min;
+		$b = $max;
+	}elsif($h < 360){
 		$r = $max;
 		$g = $min;
 		$b = $min + ($max-$min)*(360-$h)/60;
+	}else{
+		$r = 1;
+		$g = 1;
+		$b = 1;
 	}
 	
 	$r = floor($r*255);
@@ -140,107 +152,91 @@ foreach my $filename (@allfile) {
 		if($d =~ /(rgba)\((\d+),\s*(\d+),\s*(\d+),\s*(\d?\.?\d+)\)/){
 			my $color = $filename."\t".$1."\t".$2."\t".$3."\t".$4."\t".$5;
 			
-			my $hsl = rgb2hsl($2, $3, $4);
+			my $red = $2;
+			my $green = $3;
+			my $blue = $4;
+			my $alpha = $5;
+			
+			my $hsl = rgb2hsl($red, $green, $blue);
 			my $H = floor($hsl->{h});
 			my $S = floor($hsl->{s});
 			my $L = floor($hsl->{l});
 			
-			my $rgb = hsl2rgb($H, $S/100, $L/100);
-			my $rk = $rgb->{r};
-			my $gk = $rgb->{g};
-			my $bk = $rgb->{b};
+			my $rgbtest = hsl2rgb($H, $S/100, $L/100);
+			my $rtest = $rgbtest->{r};
+			my $gtest = $rgbtest->{g};
+			my $btest = $rgbtest->{b};
 			
-
+			my $hc = $H;
+			my $sc = $S;
+			my $lc = $L;
+			my $class = "---";
 			
-			print($2.",".$3.",".$4." -> ".$H.",".$S.",".$L."->".$rk.",".$gk.",".$bk."\n");
-#			print($2.",".$3.",".$4." -> ".$H.",".$S.",".$L."\n");
-
-			# $H $S $L -> $HC $SC $LS ######################################
-			my $red = $2; 
-			my $green = $3;
-			my $blue = $4;
-			my $r = $red;
-			my $g = $green;
-			my $b = $blue;
-			my $class = 0;
+			# 変換式 ######################################
 			
-			if(($red == $green) && ($red == $blue) ){ #モノクロ系統
+			if( $S < 1 ){ #モノクロ系統
 				if($d =~ /text-/){ #注記（text-color, text-halo-color）
-					$r = $red;
-					$g = $red;
-					$b = $red;
-					$class = 100;
-				}elsif($red > 254){ #白部分
-					$r = $red;
-					$g = $red;
-					$b = $red;
-					$class = -100;
-				}elsif($red > 169){ #薄い部分
-					$r = $red;
-					$g = 255;
-					$b = floor(($red-145)*255/110);
-					$class = -100;
+					$class = "text";
+					$hc = $H;
+					$sc = $S;
+					$lc = $L;
+				}elsif($L > 99){ #白部分
+					$hc = $H;
+					$sc = $S;
+					$lc = $L;
+					$class = "mono1";
+				}elsif($L > 65){ #薄い部分
+					$hc = $H + 60;
+					$sc = $S + 50;
+					$lc = $L;
+					$class = "mono2";
 				}else{
-					$r = $red;
-					$g = $red;
-					$b = $red;
-					$class = -100;
+					$hc = $H;
+					$sc = $S;
+					$lc = $L;
+					$class = "mono3";
 				}
-			}elsif( ($red >= $green) && ($red > $blue) ){ #赤系統（r=gの黄色系統含む）
-				if($red == 200){ #rgba(200,160,60,1) --- 地下・トンネル、等高線の色は印象が大きいので、指定してもよいかもしれない
-					$r = $red;
-					$g = $green;
-					$b = $blue;
-					$class = -102;
-				}else{
-					$r = 255;
-					$g = 100;
-					$b = 100;
-					$class = 1;
-				}
-			}elsif( ($green >= $red) && ($green > $blue) ){ #緑系統（g=bの蛍光っぽい水色系統含む）
-				if($d =~ /text-/){ #注記（text-color, text-halo-color） --- 緑系統のtextは1色だけなので、決め打ちでもよい
-					$r = $red;
-					$g = $green;
-					$b = $blue;
-					$class = 101;
-				}else{
-					$r = 0 ;
-					$g = 140;
-					$b = 0;
-					$class = 1;
-				}
-			}else{ #青～紫系統
-				if($red == 190){ #rgba(190,210,255,1) --- 水域の色は印象が大きいので、指定してもよいかもしれない。
-					$r = 255;
-					$g = 255;
-					$b = 200;
-					$class = -103;
-				}else{
-					$r = $red;
-					$g = $green;
-					$b = $blue;
-					$class = 1;
-				}
+			}elsif( ($red == 19 ) && ($green == 97 ) && ($blue == 69 ) ){ #緑―注記
+				$hc = $H + 60;
+				$sc = $S;
+				$lc = $L;
+				$class = "text-green";
+			}elsif( ($red == 190) && ($green == 210) && ($blue == 255) ){ #水域
+				$hc = $H + 60;
+				$sc = $S;
+				$lc = $L;
+				$class = "water";
+			}elsif( ($red == 200) && ($green == 160) && ($blue == 60 ) ){ #等高線・トンネル
+				$hc = $H + 60;
+				$sc = $S;
+				$lc = $L;
+				$class = "contour";
+			}else{ #その他の色
+				$hc = $H + 60;
+				$sc = $S;
+				$lc = $L;
+				$class = "color";
 			}
 			
-			#memo
-			# $r = $red > 150 ? $red : 150;
-			# 255 - ceil(($red + $green + $blue)/3);
-			# floor(($red + $green + $blue)/3);
 			###############################################################
 			
+			my $rgb = hsl2rgb($hc, $sc/100, $lc/100);
+			my $r = $rgb->{r};
+			my $g = $rgb->{g};
+			my $b = $rgb->{b};
 			
-			my $code = "\$d =~ s/".$1."\\(".$2.",\\s*".$3.",\\s*".$4.",?\\s*".$5."\\)/hsla\\(".$H.",".$S."%,".$L."%,".$5."\\)/g;print OutFH \$d;next;";
-			if($class > 99){
-				$code = "if\( \( \$d =~ /text-/\ \) && \( \$d =~ /".$1."\\(".$2.",\\s*".$3.",\\s*".$4.",?\\s*".$5."\\)/\ \) \)\{".$code."\}\n";
+			print($red.",".$green.",".$blue." : ".$H.",".$S."%,".$L."% : ".$rtest.",".$gtest.",".$btest." -> ".$hc.",".$sc."%,".$lc."% : ".$r.",".$g.",".$b."\n");
+			
+			my $code = "\$d =~ s/".$1."\\(".$red.",\\s*".$green.",\\s*".$blue.",?\\s*".$alpha."\\)/rgba\\(".$r.",".$g.",".$b.",".$alpha."\\)/g;print OutFH \$d;next;";
+			if($class =~ "text"){
+				$code = "if\( \( \$d =~ /text-/\ \) && \( \$d =~ /".$1."\\(".$red.",\\s*".$green.",\\s*".$blue.",?\\s*".$alpha."\\)/\ \) \)\{".$code."\}\n";
 			}else{
-				$code = "if\( \( \$d =~ /".$1."\\(".$2.",\\s*".$3.",\\s*".$4.",?\\s*".$5."\\)/\ \) \)\{".$code."\}\n";
+				$code = "if\( \( \$d =~ /".$1."\\(".$red.",\\s*".$green.",\\s*".$blue.",?\\s*".$alpha."\\)/\ \) \)\{".$code."\}\n";
 			}
 			#のちのち、ソート順で、$d =~ /text-color/ を先に処理させる。
 			
-			my $rgba = "<tr><td>"."rgba(".$2.",".$3.",".$4.",".$5.")"."</td><td><span style='background:rgba(".$2.",".$3.",".$4.",".$5.");'>"."_________"."</span></td><td>"."rgba(".$r.",".$g.",".$b.",".$5.")"."</td><td><span style='background:rgba(".$r.",".$g.",".$b.",".$5.");'>"."__________"."</span></td></tr>"."\n";
-			my $hsla = "<tr><td>"."rgba(".$2.",".$3.",".$4.",".$5.")"."</td><td><span style='background:rgba(".$2.",".$3.",".$4.",".$5.");'>"."_________"."</span></td><td>"."hsla(".$H.",".$S."%,".$L."%,".$5.")"."</td><td><span style='background:hsla(".$H.",".$S."%,".$L."%,".$5.");'>"."__________"."</span></td></tr>"."\n";
+			my $rgba = "<tr><td>".$class."</td><td>rgba(".$red.",".$green.",".$blue.",".$alpha.")"."</td><td><span style='background:rgba(".$red.",".$green.",".$blue.",".$alpha.");'>"."_________"."</span></td><td>"."hsla(".$H.",".$S."%,".$L."%,".$alpha.")"."</td><td><span style='background:hsla(".$H.",".$S."%,".$L."%,".$alpha.");'>"."__________"."</span></td><td>"."hsla(".$hc.",".$sc."%,".$lc."%,".$alpha.")"."</td><td><span style='background:hsla(".$hc.",".$sc."%,".$lc."%,".$alpha.");'>"."__________"."</span></td><td>"."rgba(".$r.",".$g.",".$b.",".$alpha.")"."</td><td><span style='background:rgba(".$r.",".$g.",".$b.",".$alpha.");'>"."__________"."</span></td></tr>"."\n";
+			my $hsla = "<tr><td>"."rgba(".$red.",".$green.",".$blue.",".$alpha.")"."</td><td><span style='background:rgba(".$red.",".$green.",".$blue.",".$alpha.");'>"."_________"."</span></td><td>"."hsla(".$H.",".$S."%,".$L."%,".$alpha.")"."</td><td><span style='background:hsla(".$H.",".$S."%,".$L."%,".$alpha.");'>"."__________"."</span></td></tr>"."\n";
 
 			$results{$color}++;
 			$perlcode{$code}++;
@@ -251,6 +247,7 @@ foreach my $filename (@allfile) {
 	close(FH);
 }
 
+#統計
 my $outfilename = "results/results2_".time."\.txt";
 open (OutFH, ">", $outfilename) or die "$!";
 foreach my $key (sort{$a cmp $b} keys(%results)){
@@ -258,6 +255,7 @@ foreach my $key (sort{$a cmp $b} keys(%results)){
 }
 close(OutFH);
 
+#Perlコード（コピペ・保存用）
 my $outfilecode = "results/perlcode_".time."\.txt";
 open (OutFH, ">", $outfilecode) or die "$!";
 foreach my $key (sort{$a cmp $b} keys(%perlcode)){
@@ -265,6 +263,7 @@ foreach my $key (sort{$a cmp $b} keys(%perlcode)){
 }
 close(OutFH);
 
+#rgb = hsl -> hsl = rgb　変換確認
 my $outfileclist = "results/colorlist_".time."\.html";
 open (OutFH, ">", $outfileclist) or die "$!";
 print OutFH "<table>\n";
@@ -274,7 +273,7 @@ foreach my $key (sort{$b cmp $a} keys(%colorlist)){
 print OutFH "</table>\n";
 close(OutFH);
 
-
+#Perlコード（実行用・一時的）
 my $outfilecode = "tmp_perlcode.txt";
 open (OutFH, ">", $outfilecode) or die "$!";
 foreach my $key (sort{$b cmp $a} keys(%perlcode)){
@@ -283,7 +282,7 @@ foreach my $key (sort{$b cmp $a} keys(%perlcode)){
 }
 close(OutFH);
 
-
+#
 open (OutFH, ">", "hsllist.html") or die "$!";
 print OutFH "<table>\n";
 foreach my $key (sort{$b cmp $a} keys(%hsllist)){
@@ -297,3 +296,7 @@ print  "\n";
 print time;
 print  "\n";
 print time -  $start;  # 経過時間を出力
+print  "\n---\n";
+print floor(0.55);
+print  "\n";
+print ceil(-0.55);
